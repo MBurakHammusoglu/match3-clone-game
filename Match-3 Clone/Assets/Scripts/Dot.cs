cbf129aa862fc1a01df5ca19658329a20becdf72
Burak Hammusoglu <m.burakhammusoglu@gmail.com>
2023-04-27 20:58:49 +0300

    Swipe Mechanics
    Swipe mechanic has been added to the game.

--- Match-3 Clone/Assets/Scripts/Dot.cs
+++ Match-3 Clone/Assets/Scripts/Dot.cs
@@ -4,12 +4,55 @@ using UnityEngine;
 
 public class Dot : MonoBehaviour
 {
+    public int column;
+    public int row;
+    public int targetX;
+    public int targetY;
+    private Board board;
+    private GameObject otherDot;
     private Vector2 firstTouchPosition;
     private Vector2 finalTouchPosition;
+    private Vector2 tempPosition;
     public float swipeAngle = 0;
     void Start()
     {
-        
+        board = FindObjectOfType<Board>();
+        targetX = (int)transform.position.x;
+        targetY = (int)transform.position.y;
+        row = targetY;
+        column = targetX;
+
+    }
+    private void Update()
+    {
+        targetX = column;
+        targetY = row;
+        if(Mathf.Abs(targetX-transform.position.x) > .1)
+        {
+            // Move towards the target
+            tempPosition = new Vector2(targetX, transform.position.y);
+            transform.position = Vector2.Lerp(transform.position, tempPosition, .4f);
+        }
+        else
+        {
+            // Directly set the position
+            tempPosition = new Vector2(targetX, transform.position.y);
+            transform.position = tempPosition;
+            board.allDots[column, row] = this.gameObject;
+        }
+          if(Mathf.Abs(targetY-transform.position.y) > .1)
+        {
+            // Move towards the target
+            tempPosition = new Vector2(transform.position.x, targetY);
+            transform.position = Vector2.Lerp(transform.position, tempPosition, .4f);
+        }
+        else
+        {
+            // Directly set the position
+            tempPosition = new Vector2(transform.position.x, targetY);
+            transform.position = tempPosition;
+            board.allDots[column, row] = this.gameObject;
+        }
     }
 
 
@@ -30,6 +73,41 @@ public class Dot : MonoBehaviour
     {
         swipeAngle = Mathf.Atan2(finalTouchPosition.y - firstTouchPosition.y, finalTouchPosition.x - firstTouchPosition.x) * 180 / Mathf.PI;
         Debug.Log(swipeAngle);
+        MovePieces();
+    }
+    void MovePieces()
+    {
+        if(swipeAngle> -45 && swipeAngle <= 45 && column <board.width)
+        {
+            // Right swipe
+            otherDot = board.allDots[column + 1, row];
+            otherDot.GetComponent<Dot>().column -= 1;
+            column += 1;
+
+        }else if (swipeAngle > 45 && swipeAngle <= 135 && row <board.height)
+        {
+            // Up swipe
+            otherDot = board.allDots[column, row +1];
+            otherDot.GetComponent<Dot>().row -= 1;
+            row += 1;
+
+        }
+        else if ((swipeAngle > 135 || swipeAngle <=-135 ) && column >0 )
+        {
+            // Left swipe
+            otherDot = board.allDots[column -1, row];
+            otherDot.GetComponent<Dot>().column += 1;
+            column -= 1;
+
+        }
+        else if (swipeAngle < -45 && swipeAngle >= -135 && row >0)
+        {
+            // Down swipe
+            otherDot = board.allDots[column, row -1];
+            otherDot.GetComponent<Dot>().row += 1;
+            row -= 1;
+
+        }
     }
 
 
