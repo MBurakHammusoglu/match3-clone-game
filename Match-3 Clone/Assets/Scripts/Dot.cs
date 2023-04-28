24952d7cadf87f08096db92c721893e6a5fd3c68
Burak Hammusoglu <m.burakhammusoglu@gmail.com>
2023-04-28 15:04:43 +0300

    Update Dot
    * Fixed dots swipe.

--- Match-3 Clone/Assets/Scripts/Dot.cs
+++ Match-3 Clone/Assets/Scripts/Dot.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class Dot : MonoBehaviour
 {
+    [Header("Board Variables")]
     public int column;
     public int row;
+    public int previousColumn;
+    public int previousRow;
     public int targetX;
     public int targetY;
     public bool isMatched = false;
+
     private Board board;
     private GameObject otherDot;
     private Vector2 firstTouchPosition;
@@ -22,6 +26,8 @@ public class Dot : MonoBehaviour
         targetY = (int)transform.position.y;
         row = targetY;
         column = targetX;
+        previousRow = row;
+        previousColumn = column;
 
     }
     private void Update()
@@ -61,6 +67,21 @@ public class Dot : MonoBehaviour
             board.allDots[column, row] = this.gameObject;
         }
     }
+    public IEnumerator CheckMoveCo()
+    {
+        yield return new WaitForSeconds(.5f);
+        if(otherDot != null)
+        {
+            if (!isMatched && !otherDot.GetComponent<Dot>().isMatched)
+            {
+                otherDot.GetComponent<Dot>().row = row;
+                otherDot.GetComponent<Dot>().column = column;
+                row = previousRow;
+                column = previousColumn;
+            }
+            otherDot = null;
+        }
+    }
 
 
 
@@ -84,14 +105,14 @@ public class Dot : MonoBehaviour
     }
     void MovePieces()
     {
-        if(swipeAngle> -45 && swipeAngle <= 45 && column <board.width)
+        if(swipeAngle> -45 && swipeAngle <= 45 && column <board.width-1)
         {
             // Right swipe
             otherDot = board.allDots[column + 1, row];
             otherDot.GetComponent<Dot>().column -= 1;
             column += 1;
 
-        }else if (swipeAngle > 45 && swipeAngle <= 135 && row <board.height)
+        }else if (swipeAngle > 45 && swipeAngle <= 135 && row <board.height-1)
         {
             // Up swipe
             otherDot = board.allDots[column, row +1];
@@ -115,6 +136,7 @@ public class Dot : MonoBehaviour
             row -= 1;
 
         }
+        StartCoroutine(CheckMoveCo());
     }
 
     void FindMatches()
